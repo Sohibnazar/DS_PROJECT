377000f3ffe770416ced76da16bb4b661ac88528
Sohibnazar <115521214+Sohibnazar@users.noreply.github.com>
2025-11-21 20:20:14 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,7 @@ with st.sidebar:
   st.header('Input features')
   island = st.selectbox('**Island**', {'Torgesen', 'Dream', 'Biscoe'})
   bill_length_mm = st.slider('bill_length_mm', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('bill_depth_mm', 32.1, 59.6, 43.9)
+  flipper_length_mm = st.slider('flipper_length_mm', 32.1, 59.6, 43.9)
+  body_mass_g = st.slider('body_mass_g', 32.1, 59.6, 43.9) 
+  sex = st.selectbox('**Gender**', {'Male', 'Femail' })
