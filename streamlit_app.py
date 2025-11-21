7d2101814d158bd97ff9b0ec74283a06b9f0aeee
Sohibnazar <115521214+Sohibnazar@users.noreply.github.com>
2025-11-21 19:59:17 +0500

    Add loading of penguins dataset
    Load penguins dataset from a CSV file.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,6 @@ st.title('😂😂😂MY FIRST PROJECT')
 st.write('Lets do it!')
 with st.expander("Data"):
   st.write('**OUR DATA**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
   
