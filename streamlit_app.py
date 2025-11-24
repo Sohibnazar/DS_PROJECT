e4ab059d005e3966ae25c82a8cfb2dac8bfcab87
Sohibnazar <115521214+Sohibnazar@users.noreply.github.com>
2025-11-24 19:00:03 +0500

    Enhance UI with expander for input features
    Add expander to display input raw data

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,9 @@ data = {'island': island,
 }
 input_df = pd.DataFrame(data, index=[0])
 input_penguins = pd.concat([input_df, X_raw,], axis=0)
+
+with st.expander("Input features"):
+  st.write("**Input raw**")
+  input_df
+
+
